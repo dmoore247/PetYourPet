e90a66bab7fa8065eea04662b2d4dc762e9c184f
Douglas Moore <Douglas.Moore@ThinkBigAnalytics.com>
2014-05-25 18:33:43 -0400

    add device properties...

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,13 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+	 var element = document.getElementById('deviceProperties');
+
+        element.innerHTML = 'Device Name: '     + device.name     + '<br />' + 
+                            'Device PhoneGap: ' + device.phonegap + '<br />' + 
+                            'Device Platform: ' + device.platform + '<br />' + 
+                            'Device UUID: '     + device.uuid     + '<br />' + 
+                            'Device Version: '  + device.version  + '<br />';
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
